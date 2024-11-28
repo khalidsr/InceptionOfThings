9e2f307831fd06d730b5f62af672a305c8f877b8
sriyani <sriyani@milan.myguest.virtualbox.org>
2024-11-28 16:13:34 +0100

    still need to fix some issues in p1

--- p1/scripts/worker_script.sh
+++ p1/scripts/worker_script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update -y
 sudo apt-get install -y curl
 echo "Waiting for /vagrant/node-token from the controller"
+
 while [ ! -f /vagrant/node-token ]; do
   sleep 2
 done
@@ -10,7 +13,7 @@ done
 
 NODE_TOKEN=$(cat /vagrant/node-token)
 SERVER_IP="192.168.56.110"
-
+echo ${NODE_TOKEN}"--------------"
 curl -sfL https://get.k3s.io | K3S_URL=https://${SERVER_IP}:6443 K3S_TOKEN=${NODE_TOKEN} sh -
 
 
--- p2/scripts/server_script.sh
+++ p2/scripts/server_script.sh
@@ -5,7 +5,4 @@ apt-get update -y
 sudo apt-get install -y curl
 curl -sfL https://get.k3s.io | sh -
 
-
-echo "Saving K3s node token to /vagrant"
-sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 echo "done"
