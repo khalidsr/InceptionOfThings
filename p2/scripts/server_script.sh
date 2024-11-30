428fad0cbbe18ed047f6f033cc5a6ffe7178f7e3
sriyani <sriyani@IOT.myguest.virtualbox.org>
2024-11-30 20:25:20 +0100

    continue in part2

--- p2/scripts/server_script.sh
+++ p2/scripts/server_script.sh
@@ -2,9 +2,25 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
-sudo apt-get install -y curl
+apt-get install -y curl
+
 curl -sfL https://get.k3s.io | sh -
-cp /* .
 
 
-echo "done"
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+echo "Waiting for K3s to be ready..."
+while ! sudo kubectl get nodes &> /dev/null; do
+    sleep 5
+done
+echo "K3s is ready."
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+echo "Applying Kubernetes manifests..."
+kubectl apply -f ./apps/app1/app1.yaml --validate=false
+kubectl apply -f ./apps/app2/app2.yaml --validate=false
+kubectl apply -f ./apps/app3/app3.yaml --validate=false
+kubectl apply -f ingress.yaml --validate=false
+
+echo "Deployment completed."
