d930db62e7a5a62f22554a4c2f946ec561fe0375
dependentmadani <madani.badaoui12@gmail.com>
2024-12-01 18:36:15 +0100

    update : few modification done, still need to be tested

--- p1/serverWorker_script.sh
+++ p1/serverWorker_script.sh
@@ -1,1 +1,15 @@
-# script to be written
+#!/bin/bash
+
+sudo apt-get update -y
+
+sudo apt-get install curl -y
+
+curl -sfL https://get.k3s.io | sh -
+
+K3S_URL="https://192.168.56.110:6443/"
+
+TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+
+curl -sfL https://get.k3s.io/ | K3S_URL=$K3S_URL K3S_TOKEN=$TOKEN sh -
+
+echo "Everthing is well setup!"
--- p1/server_script.sh
+++ p1/server_script.sh
@@ -1,1 +1,13 @@
-# script to be written
+#! /bin/bash
+
+sudo apt-get update -y
+
+sudo apt-get install curl -y
+
+curl -sfL https://get.k3s.io | sh -
+
+sudo apt-get install snapd -y
+
+sudo snap install kubectl --classic
+
+echo "Everthing installed correctly!"
