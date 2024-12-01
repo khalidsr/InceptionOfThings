446d14b8a98db5b64fea00fc847c77f33739b832
sriyani <sriyani@IOT.myguest.virtualbox.org>
2024-12-01 19:57:05 +0100

    p2 should test confs file

--- p1/scripts/worker_script.sh
+++ p1/scripts/worker_script.sh
@@ -13,7 +13,6 @@ done
 
 NODE_TOKEN=$(cat /vagrant/node-token)
 SERVER_IP="192.168.56.110"
-echo ${NODE_TOKEN}"--------------"
 curl -sfL https://get.k3s.io | K3S_URL=https://${SERVER_IP}:6443 K3S_TOKEN=${NODE_TOKEN} sh -
 
 
--- p2/scripts/server_script.sh
+++ p2/scripts/server_script.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
+
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get install -y curl
+apt install net-tools -y
 
+MASTER_IP="192.168.56.110"
+export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644  --advertise-address=${MASTER_IP} --node-ip=${MASTER_IP}"
 curl -sfL https://get.k3s.io | sh -
 
-
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
+source ~/.bashrc
+
 
 echo "Waiting for K3s to be ready..."
-while ! sudo kubectl get nodes &> /dev/null; do
+while ! kubectl get nodes &> /dev/null; do
     sleep 5
 done
 echo "K3s is ready."
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml --validate=false
+
 
 echo "Applying Kubernetes manifests..."
 kubectl apply -f ./apps/app1/app1.yaml --validate=false
