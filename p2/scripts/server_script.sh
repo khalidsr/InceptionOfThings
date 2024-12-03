161183250bcad3cf8b82ac9d2fe77ece416403ff
sriyani <sriyani@IOT.myguest.virtualbox.org>
2024-12-03 20:13:18 +0100

    p2 done

--- p2/scripts/server_script.sh
+++ p2/scripts/server_script.sh
@@ -6,13 +6,13 @@ apt-get update -y
 apt-get install -y curl
 apt install net-tools -y
 
-MASTER_IP="192.168.56.110"
-export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644  --advertise-address=${MASTER_IP} --node-ip=${MASTER_IP}"
+# MASTER_IP="192.168.56.110"
+# export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644  --advertise-address=${MASTER_IP} --node-ip=${MASTER_IP}"
 curl -sfL https://get.k3s.io | sh -
 
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
-source ~/.bashrc
+# export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+# echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
+# source ~/.bashrc
 
 
 echo "Waiting for K3s to be ready..."
@@ -22,10 +22,13 @@ done
 echo "K3s is ready."
 
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml --validate=false
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml --validate=false
 
 
 echo "Applying Kubernetes manifests..."
+kubectl apply -f ./conf/configmap_app1.yaml --validate=false
+kubectl apply -f ./conf/configmap_app2.yaml --validate=false
+kubectl apply -f ./conf/configmap_app3.yaml --validate=false
 kubectl apply -f ./apps/app1/app1.yaml --validate=false
 kubectl apply -f ./apps/app2/app2.yaml --validate=false
 kubectl apply -f ./apps/app3/app3.yaml --validate=false
