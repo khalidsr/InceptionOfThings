5b5d86bf2e47cc856eff18eeadd9d84c0b3de0bf
dependentmadani <madani.badaoui12@gmail.com>
2024-12-05 16:48:41 +0100

    update: virtual machines with vagrant file configuration works fine, issue with k3s service in worker machine

--- p1/serverWorker_script.sh
+++ p1/serverWorker_script.sh
@@ -6,16 +6,16 @@ sudo apt-get update -y
 
 sudo apt-get install curl -y
 
-#while [[ ! -e /vagrant/node-token ]] 
-#do
-#	sleep 1
-#done
+while [[ ! -e /vagrant/node-token ]] 
+do
+	sleep 1
+done
 
 curl -sfL https://get.k3s.io | sh -
 
 K3S_URL="https://192.168.56.110:6443/"
 
-TOKEN=$(sudo cat /vagrant/node-token)
+TOKEN=$(cat /vagrant/node-token)
 
 curl -sfL https://get.k3s.io/ | K3S_URL=$K3S_URL K3S_TOKEN=$TOKEN sh -
 
