ff26d06b8db6064f8123280feaa93bd3e5c0ff98
sriyani <sriyani@IOT.myguest.virtualbox.org>
2024-12-06 21:52:55 +0100

    bonus need to complete gitlab-value

---
+++ bonus/scripts/script1.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y curl wget apt-transport-https ca-certificates software-properties-common
+
+
+echo -e "Installing Docker..."
+if ! command -v docker &> /dev/null; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+else
+    echo -e "Docker is already installed."
+fi
+
+
+echo -e "Installing K3d..."
+if ! command -v k3d &> /dev/null; then
+    curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+    
+else
+    echo -e "K3d is already installed"
+fi
+
+
+if ! command -v kubectl &> /dev/null; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/
+else
+    echo -e "kubectl is already installed."
+fi
+
+if ! command -v helm &> /dev/null; then
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    helm repo add stable https://charts.helm.sh/stable
+    helm repo update
+else
+    echo -e "helm is already installed."
+fi
+
+
+
+echo "Adding GitLab Helm repository..."
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+helm show values gitlab/gitlab > gitlab-values.yaml
+
+
+
+echo -e "Setup completed successfully!"
