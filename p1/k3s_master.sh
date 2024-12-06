0d4f2bcb6d8cff0c0362bf10355df3f97840a967
abde-r <mrz3ln@z3ln.z3ln>
2024-12-06 06:23:29 -0500

    fixing: server worker unable to connect to k3s.service

--- p1/k3s_master.sh
+++ p1/k3s_master.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt-get update -y
 sudo apt-get install -y curl
 
-# curl -sfL https://get.k3s.ioL | sh -s - server --write-kubeconfig-mode 644
+#  curl -sfL https://get.k3s.ioL | sh -s - server --write-kubeconfig-mode 644
 K3S_INSTALL_URL="https://get.k3s.io"
 KUBECONFIG_MODE=644
 curl -sfL $K3S_INSTALL_URL | sh -s - server --write-kubeconfig-mode $KUBECONFIG_MODE
