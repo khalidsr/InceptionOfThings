d99501c262a3de2459b6c98e194ee67cf60e82d3
dependentmadani <madani.badaoui12@gmail.com>
2024-12-07 15:34:20 +0100

    update: now p1 is working as it should be, stupid issue resolved

--- p1/serverWorker_script.sh
+++ p1/serverWorker_script.sh
@@ -6,17 +6,15 @@ sudo apt-get update -y
 
 sudo apt-get install curl -y
 
-while [[ ! -e /vagrant/node-token ]] 
+while [ ! -e /vagrant/node-token ] 
 do
 	sleep 1
 done
-
-curl -sfL https://get.k3s.io | sh -
 
 K3S_URL="https://192.168.56.110:6443"
 
 TOKEN=$(cat /vagrant/node-token)
 
-curl -sfL https://get.k3s.io/ | K3S_URL=$K3S_URL K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=${K3S_URL} K3S_TOKEN=${TOKEN} sh -
 
 echo "Everthing is well setup!"
