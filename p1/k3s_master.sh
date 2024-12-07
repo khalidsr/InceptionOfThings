c53ca3aaf8e10410d96a74ba6e958109bd3a4bb1
abde-r <elasri9999@gmail.com>
2024-12-07 14:45:22 -0500

    in progress: hosting web apps using k3s

--- p1/k3s_master.sh
+++ p1/k3s_master.sh
@@ -4,10 +4,7 @@ set -e
 sudo apt-get update -y
 sudo apt-get install -y curl
 
-# curl -sfL https://get.k3s.ioL | sh -s - server --write-kubeconfig-mode 644
-K3S_INSTALL_URL="https://get.k3s.io"
-KUBECONFIG_MODE=644
-curl -sfL $K3S_INSTALL_URL | sh -s - server --write-kubeconfig-mode $KUBECONFIG_MODE
+curl -sfL https://get.k3s.io | sh -
 sudo k3s kubectl get nodes
 
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
--- p1/k3s_worker.sh
+++ p1/k3s_worker.sh
@@ -9,6 +9,5 @@ while [ ! -f /vagrant/node-token ]; do
 done
 
 K3S_TOKEN=$(cat /vagrant/node-token)
-echo "Token retrieved: $K3S_TOKEN"
 curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="$K3S_TOKEN" sh -
 echo "k3s agent setup complete."
