eeb65989ae5ebabeeb6e678f37790bd3f019dec9
dependentmadani <madani.badaoui12@gmail.com>
2024-12-09 14:56:38 +0100

    update: p2 finally starts working

--- p2/server_script.sh
+++ p2/server_script.sh
@@ -14,4 +14,17 @@ sudo apt-get install snapd -y
 
 sudo snap install kubectl --classic
 
+#commands to run deployment
+sudo kubectl apply -f /vagrant/deployment/app1.deployment.yaml
+sudo kubectl apply -f /vagrant/deployment/app2.deployment.yaml
+sudo kubectl apply -f /vagrant/deployment/app3.deployment.yaml
+
+#commands to run services
+sudo kubectl apply -f /vagrant/apps/app1.service.yaml
+sudo kubectl apply -f /vagrant/apps/app2.service.yaml
+sudo kubectl apply -f /vagrant/apps/app3.service.yaml
+
+#command to apply the ingress
+sudo kubectl apply -f /vagrant/ingress.yaml
+
 echo "Everthing installed correctly!"
